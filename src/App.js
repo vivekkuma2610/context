8c98cb26fd23881b499f03e4c4ac120e9dcab4c6
vivekkuma2610 <vivekpanneer2610@gmail.com>
2023-11-28 02:02:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import CartComponent from './components/CartComponent';
+import Total from './components/Total';
+import {useState } from 'react'
 
 function App() {
+    
+  const[totalQuantity,setTotalQuantityContext] = useState(5)
+
+  const [quantity,setQuantity]  = useState(1)
+
+  const [totalPrice,setTotalPrice]  = useState(3476)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <CartComponent totalPrice={totalPrice}setTotalPrice={setTotalPrice}  quantity={quantity} setQuantity = {setQuantity} totalQuantity={totalQuantity} setTotalQuantityContext={setTotalQuantityContext}/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import logo from './logo.svg';
+import './App.css';
+import CartComponent from './components/CartComponent';
+import Total from './components/Total';
+import {useState } from 'react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+    
+  const[totalQuantity,setTotalQuantityContext] = useState(5)
+
+  const [quantity,setQuantity]  = useState(1)
+
+  const [totalPrice,setTotalPrice]  = useState(3476)
+
+  return (
+    <div className="App">
+     <CartComponent totalPrice={totalPrice}setTotalPrice={setTotalPrice}  quantity={quantity} setQuantity = {setQuantity} totalQuantity={totalQuantity} setTotalQuantityContext={setTotalQuantityContext}/>
+    </div>
+  );
+}
+
+export default App;
