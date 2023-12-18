60bbb4295fb221c5409d1a2054a12d50d95c10db
vivekkuma2610 <vivekpanneer2610@gmail.com>
2023-12-18 15:55:10 +0530

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import CartComponent from './components/CartComponent';
-import Total from './components/Total';
-import {useState } from 'react'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-function App() {
-    
-  const[totalQuantity,setTotalQuantityContext] = useState(5)
-
-  const [quantity,setQuantity]  = useState(1)
-
-  const [totalPrice,setTotalPrice]  = useState(3476)
-
-  return (
-    <div className="App">
-     <CartComponent totalPrice={totalPrice}setTotalPrice={setTotalPrice}  quantity={quantity} setQuantity = {setQuantity} totalQuantity={totalQuantity} setTotalQuantityContext={setTotalQuantityContext}/>
-    </div>
-  );
-}
-
-export default App;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
